03be3de1959cba8d9c9243930931a0f4cd843361
nelson16q <nelson16q@hotmail.com>
2016-08-03 20:45:46 -0500

    primer despliegue en server

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
       <title>JW Social Showroom</title>
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
       <link href="css/advanced-eggBlue.css" rel="stylesheet" type="text/css" media="all"/>
+      <link href="css/isloading.css" rel="stylesheet" type="text/css" />
+      <link href="css/colorbox.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
       <section class="animsition">
@@ -188,24 +190,25 @@
 
               
 
-              <form id="contact_form" class="row"  action="<?php echo "http://" . $_SERVER['SERVER_NAME'] .  '/JW-Social-Showroom/registro.php'; ?>" method="POST" >
+              <form id="contact_form" class="row"  action="<?php echo "http://" . $_SERVER['SERVER_NAME'] .  '/registro.php'; ?>" method="POST" >
                 <div class="col-sm-12 col-xs-12">
-                  <input type="name" name="name_user" class="input-name" placeholder="Nombre Completo">
-                  <input type="email" name="email_user" class="input-email" placeholder="Correo Electrónico">
+                  <input type="name" name="name_user" class="input-name" placeholder="Nombre Completo" required>
+                  <input type="email" name="email_user" class="input-email" placeholder="Correo Electrónico" required>
                 </div>
                 <div class="col-sm-12 col-xs-12">
-                  <input type="tel" name="tel_user" class="input-name" placeholder="Número Celular">
+                  <input type="tel" name="tel_user" class="input-name" placeholder="Número Celular" required>
                   <input type="input" name="user_social" class="input-name" placeholder="Usuario de twitter, Facebook o Instagram">
                 </div>
                 
                 <div class="col-sm-12 col-xs-12">
-                  <button class="submit">Inscribirse</button>
+                  <button class="submit" type="button" name="Inscribirse" id="send_form_social_show">Inscribirse</button>
+                  <!--<input  class="submit" type="button" name="Inscribirse" id="send_form_social_show">-->
                 </div>
 
-                <div id="form-messages" class="col-sm-12 col-xs-12">
+                <!--<div id="form-messages" class="col-sm-12 col-xs-12">
                   <span class="success col-sm-12 col-xs-12"></span>
                   <span class="error col-sm-12 col-xs-12"></span>
-                </div>
+                </div>-->
               </form>
           </div>
 
@@ -286,6 +289,8 @@
         </div>
       </div>
 
+      
+
       <script src="js/jquery-1.11.3.min.js"></script>
       <script src="js/bootstrap.min.js"></script>
       <script src="js/animsition.min.js"></script>
@@ -300,5 +305,69 @@
       <script src="js/photoswipe-ui-default.min.js"></script>
       <script src="js/jqinstapics.min.js"></script>
       <script src="js/script.js"></script>
+
+      <script src="js/jquery.isloading.js"></script>
+      <script src="js/jquery.colorbox-min.js"></script>
+      <script src="js/jquery.validate.js"></script>
+
+      <script type="text/javascript">
+        $(document).on('ready',function(){
+
+          $("#contact_form").validate({
+            rules: {
+              name_user: "required",
+              email_user: {
+                required: true,
+                email: true
+              },
+              tel_user: {
+                required: true,
+                number: true
+              }
+            },
+            messages: {
+                name_user: "Por favor Ingrese su nombre",
+                tel_user: {
+                    required: "Por favor ingrese su teléfono",
+                    number:  "El teléfono debe ser un valor númerico"
+                },
+                email_user: {
+                    required: "Por favor ingrese su correo",
+                    email:  "Por favor ingrese un correo válido"
+                }
+            }
+            /*errorClass: "invalid",
+            validClass: "success",
+            errorPlacement: function(error, element) {}*/
+          });
+
+          $(document).on('click','#send_form_social_show',function(e){
+              e.preventDefault();
+              if($('#contact_form').valid()){
+                  //alert('si valido');
+                  $.isLoading({ text: "Por favor espere, estamos procesando su inscripción..." });
+                  $.ajax({
+                     type: 'POST',
+                     url: $('#contact_form').attr('action'),
+                     data: $('#contact_form').serialize(),
+                     success: function(data){
+                      //console.log(data); 
+                      $.isLoading( "hide" );
+
+                      $.isLoading({ text: "Su registro se ha completado con éxito, gracias por inscribirse" });
+                      $('#contact_form')[0].reset();
+                      setTimeout(function(){ $.isLoading( "hide" ); }, 3000);
+                    }
+                  });
+              }else{
+                //alert('no valido');
+              }
+
+          });
+
+
+        });
+
+      </script>
   </body>
 </html>
