effad4be233e8a9d51f29b477dea22f68ea84a71
Fabian Zapata <fabian.zapata@pklagencia.com>
2016-08-12 10:35:00 -0500

    Codigo Facebook

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
       <link href="css/isloading.css" rel="stylesheet" type="text/css" />
       <link href="css/colorbox.css" rel="stylesheet" type="text/css" />
       <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+      <!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '1746387905634067');
+      fbq('track', "PageView");</script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1746387905634067&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
     </head>
     <body>
       <section class="animsition">
